99cb8321bb0d3ac90b65e998d12c876934b52a66
hayatimaghri <hayatimaghri04@gmail.com>
2026-04-29 22:12:36 +0100

    Add repositories API integration

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ async function fetchUser(userName){
 }
 async function fetchRespo(userName) {
     try{
-        let res= await fetch('https://api.github.com/users/{username}/repos?sort=stars&per_page=5')
-        if(res.ok){
+        let res= await fetch(`https://api.github.com/users/${userName}/repos?sort=stars&per_page=5`)
+        if(!res.ok){
             throw new Error("repos not found")
         }
         let data= await res.json()
@@ -44,3 +44,106 @@ async function fetchRespo(userName) {
         return null
     }
 }
+function displayProfil(user , respo){
+    let profilContainer = document.getElementById("resultProfil");
+profilContainer.innerHTML = ""
+
+    let card = document.createElement("div");
+    card.className = "profile-card";
+
+    card.innerHTML = `
+        <div class="top">
+            <img src="${user.avatar_url}" class="avatar">
+
+            <div class="info">
+                <h2>${user.name}</h2>
+                <p>@${user.login}</p>
+                <p class="bio">${user.bio || "No bio available"}</p>
+
+                <div class="stats">
+                    <span>Followers <b>${user.followers}</b></span>
+                    <span>Following <b>${user.following}</b></span>
+                    <span>Public Repos <b>${user.public_repos}</b></span>
+                </div>
+            </div>
+
+            <button class="bookmark">★ Bookmark</button>
+        </div>
+
+        <a class="link" href="https://github.com/${user.login}" target="_blank">
+            Visit GitHub Profile →
+        </a>
+    `;
+     profilContainer.appendChild(card);
+
+     let respoContainer = document.getElementById("RepositoriesProfil");
+    respoContainer.innerHTML = "<h1>Top Repositories</h1>";
+
+    respo.forEach(respo => {
+
+        let div = document.createElement("div");
+        div.className = "repo-card";
+
+        div.innerHTML = `
+            <div class="repo-top">
+                <h3>${respo.name}</h3>
+                <span>⭐ ${respo.stargazers_count}</span>
+            </div>
+
+            <p>${respo.description || "No description"}</p>
+
+            <div class="respo-bottom">
+                <span>${respo.language}</span>
+                <a href="${respo.html_url}" target="_blank">Open</a>
+            </div>
+        `;
+
+        respoContainer.appendChild(div);
+    });
+}
+
+function showLoading() {
+    laodingStatut.style.display = "block";
+    errorStatut.style.display = "none";
+    welcomeState.style.display = "none";
+}
+function showError(message) {
+errorStatut.style.display = "block";
+errorStatut.innerText = message;
+
+    laodingStatut.style.display = "none";
+    welcomeState.style.display = "none";
+}
+function showWelcome() {
+    welcomeState.style.display = "block";
+    laodingStatut.style.display = "none";
+    errorStatut.style.display = "none";
+}
+
+searchBtn.addEventListener('click', () => {
+let userName= searchInput.value.trim()
+if ( userName !==""){
+    loaderUser(userName)
+}
+   
+});
+searchInput.addEventListener('keypress', (event) => {
+   if(event.key=== "Enter"){
+    let userName= searchInput.value.trim()
+ }
+ if ( userName !==""){
+    loaderUser(userName)
+ }
+});
+
+async function loaderUser(userName){
+try{
+    showLoading()
+    let user= await fetchUser(userName);
+    let respo= await fetchRespo(userName);
+    displayProfil(user,respo)
+    }
+    catch (error){
+            showError(error.message);
+  }
+    }
